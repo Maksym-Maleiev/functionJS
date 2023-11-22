de7748c0c584084d37c9c20414359d726475cc73
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-22 14:26:51 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -82,3 +82,14 @@ var randomWords = ["Fly", "Marmot", "Stick", "Monkey", "Rat"];
 var randomString = "Your " + pickRandomWord(randomBodyParts) + " is like a " + pickRandomWord(randomAdjectives) + " " + pickRandomWord(randomWords) + "!!!";
 
 console.log(randomString);
+
+// ---------
+generateRandomInsult = function () {
+  var randomBodyParts = ["Face", "Nose", "Hair"];
+  var randomAdjectives = ["Smelly", "Boring", "Stupid"];
+  var randomWords = ["Fly", "Marmot", "Stick", "Monkey", "Rat"];
+  
+  var randomString = "Your " + pickRandomWord(randomBodyParts) + " is like a " + pickRandomWord(randomAdjectives) + " " + pickRandomWord(randomWords) + "!!!";
+
+  return randomString;
+};
